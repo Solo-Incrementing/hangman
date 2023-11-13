9e4782c0d884712017bbf7eac264d4f5689d0b41
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-13 20:38:12 +0000

    Reformat all files

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,38 @@
 // general elements
-const contentContainers = document.getElementsByClassName('js-content-container');
+const contentContainers = document.getElementsByClassName(
+	"js-content-container"
+);
 
 // main menu elements
-const mainMenuContentContainer = document.getElementById('mainMenuContentContainer');
-const normalModeButton = document.getElementById('normalModeButton');
-const hardcoreModeButton = document.getElementById('hardcoreModeButton');
+const mainMenuContentContainer = document.getElementById(
+	"mainMenuContentContainer"
+);
+const normalModeButton = document.getElementById("normalModeButton");
+const hardcoreModeButton = document.getElementById("hardcoreModeButton");
 
 // category menu elements
-const categoryMenuContentContainer = document.getElementById('categoryMenuContentContainer');
-const categoryButtons = document.getElementsByClassName('js-category-button');
-const categoryBackButton = document.getElementById('categoryMenuBackButton');
+const categoryMenuContentContainer = document.getElementById(
+	"categoryMenuContentContainer"
+);
+const categoryButtons = document.getElementsByClassName("js-category-button");
+const categoryBackButton = document.getElementById("categoryMenuBackButton");
 
 //loading screen element
-const loadingScreenContentContainer = document.getElementById('loadingScreenContentContainer');
+const loadingScreenContentContainer = document.getElementById(
+	"loadingScreenContentContainer"
+);
 
 // main game elements
-const mainGameContainer = document.getElementById('mainGameContainer');
-const letterButtonsContainer = document.getElementById('letterButtonsContainer');
-const highScore = document.getElementById('highScore');
-const score = document.getElementById('score');
-const message = document.getElementById('message');
-const word = document.getElementById('word');
-const hangmanLimbs = document.getElementsByClassName('hangman-limb');
-const mainGameHomeButton = document.getElementById('mainGameHomeButton');
+const mainGameContainer = document.getElementById("mainGameContainer");
+const letterButtonsContainer = document.getElementById(
+	"letterButtonsContainer"
+);
+const highScore = document.getElementById("highScore");
+const score = document.getElementById("score");
+const message = document.getElementById("message");
+const word = document.getElementById("word");
+const hangmanLimbs = document.getElementsByClassName("hangman-limb");
+const mainGameHomeButton = document.getElementById("mainGameHomeButton");
 let letterButtons;
 
 // main game variables
@@ -42,254 +52,252 @@ let newGameSessionStarted = true;
 // MAIN GAME INITIALIZATION:
 
 async function initNormalMode(categoryName) {
-  goToLoadingScreen();
+	goToLoadingScreen();
 
-  await fetch('https://kriyes-m.github.io/hangman/hangman-data.json')
-    .then((response) => response.json())
-    .then((json) => {
-      wordList = json.categories[categoryName];
-      capitalizeWordList();
-    });
+	await fetch("https://kriyes-m.github.io/hangman/hangman-data.json")
+		.then((response) => response.json())
+		.then((json) => {
+			wordList = json.categories[categoryName];
+			capitalizeWordList();
+		});
 
-  setTimeout(() => {
-    initGame();
-    goToMainGame();
-  }, 2000);
+	setTimeout(() => {
+		initGame();
+		goToMainGame();
+	}, 2000);
 }
 
 async function initHardcoreMode() {
-  goToLoadingScreen();
+	goToLoadingScreen();
 
-  await fetch('https://kriyes-m.github.io/hangman/hangman-data.json')
-    .then((response) => response.json())
-    .then((json) => {
-      wordList = json.hardcoreCategories['tenThousand'];
-      capitalizeWordList();
-    });
+	await fetch("https://kriyes-m.github.io/hangman/hangman-data.json")
+		.then((response) => response.json())
+		.then((json) => {
+			wordList = json.hardcoreCategories["tenThousand"];
+			capitalizeWordList();
+		});
 
-  setTimeout(() => {
-    initGame();
-    goToMainGame();
-  }, 2000);
+	setTimeout(() => {
+		initGame();
+		goToMainGame();
+	}, 2000);
 }
 
 // MAIN GAME FUNCTIONS:
 
 function createLetterButtons() {
-  for (letterIndex in alphabet) {
-    const newLetterButton = document.createElement('button');
-    const newLetter = document.createElement('letter');
-    newLetterButton.classList.add('letter-button');
-    newLetterButton.setAttribute('data-letter', alphabet[letterIndex].toUpperCase());
-    newLetter.classList.add('letter');
-    newLetter.innerText = alphabet[letterIndex].toUpperCase();
-    newLetterButton.append(newLetter);
-    letterButtonsContainer.append(newLetterButton);
-  }
+	for (letterIndex in alphabet) {
+		const newLetterButton = document.createElement("button");
+		const newLetter = document.createElement("letter");
+		newLetterButton.classList.add("letter-button");
+		newLetterButton.setAttribute(
+			"data-letter",
+			alphabet[letterIndex].toUpperCase()
+		);
+		newLetter.classList.add("letter");
+		newLetter.innerText = alphabet[letterIndex].toUpperCase();
+		newLetterButton.append(newLetter);
+		letterButtonsContainer.append(newLetterButton);
+	}
 
-  letterButtons = document.getElementsByClassName('letter-button');
+	letterButtons = document.getElementsByClassName("letter-button");
 }
 
 function resetHangman() {
-  for (let i = 0; i < hangmanLimbs.length; ++i) {
-    hangmanLimbs[i].style.display = 'none';
-  }
+	for (let i = 0; i < hangmanLimbs.length; ++i) {
+		hangmanLimbs[i].style.display = "none";
+	}
 }
 
 function resetLetterButtons() {
-  for (let i = 0; i < letterButtons.length; ++i) {
-    letterButtons[i].style.display = 'flex';
-  }
+	for (let i = 0; i < letterButtons.length; ++i) {
+		letterButtons[i].style.display = "flex";
+	}
 }
 
 function initNewWord() {
-  if (newGameSessionStarted) {
-    newGameSessionStarted = false;
-    numberOfWordsInPlay = wordList.length;
-  }
+	if (newGameSessionStarted) {
+		newGameSessionStarted = false;
+		numberOfWordsInPlay = wordList.length;
+	}
 
-  const randomNum = Math.floor(Math.random() * numberOfWordsInPlay);
-  currentWord = wordList.splice(randomNum, 1)[0];
-  wordList.push(currentWord);
-  numberOfWordsInPlay--;
+	const randomNum = Math.floor(Math.random() * numberOfWordsInPlay);
+	currentWord = wordList.splice(randomNum, 1)[0];
+	wordList.push(currentWord);
+	numberOfWordsInPlay--;
 
-  revealedWord = "";
+	revealedWord = "";
 
-  for (let i = 0; i < currentWord.length; ++i) {
-    if (currentWord[i] !== " ") {
-      revealedWord += "_";
-    }
-    else {
-      revealedWord += " ";
-    }
-  }
-  word.innerText = revealedWord;
+	for (let i = 0; i < currentWord.length; ++i) {
+		if (currentWord[i] !== " ") {
+			revealedWord += "_";
+		} else {
+			revealedWord += " ";
+		}
+	}
+	word.innerText = revealedWord;
 }
 
 function initGame() {
-  newGameSessionStarted = true;
-  scoreValue = 0;
-  highScoreValue = localStorage.getItem('highScore');
-  updateHighScore();
-  updateScoreDisplay();
-  startGame();
+	newGameSessionStarted = true;
+	scoreValue = 0;
+	highScoreValue = localStorage.getItem("highScore");
+	updateHighScore();
+	updateScoreDisplay();
+	startGame();
 }
 
 function startGame() {
-  resetLetterButtons();
-  resetHangman();
-  initNewWord();
-  strikes = 0;
-  updateDisplay();
+	resetLetterButtons();
+	resetHangman();
+	initNewWord();
+	strikes = 0;
+	updateDisplay();
 }
 
 function initMainGameEventListeners() {
-  for (let i = 0; i < letterButtons.length; ++i) {
-    letterButtons[i].addEventListener('click', (event) => {
-      event.target.style.display = 'none';
-      message.innerText = '';
-      letterButtonClicked(event);
-    });
-  }
+	for (let i = 0; i < letterButtons.length; ++i) {
+		letterButtons[i].addEventListener("click", (event) => {
+			event.target.style.display = "none";
+			message.innerText = "";
+			letterButtonClicked(event);
+		});
+	}
 }
 
 function letterButtonClicked(event) {
-  let letterFound = false;
+	let letterFound = false;
 
-  letterFound = findLetterInWord(event.target.dataset.letter);
+	letterFound = findLetterInWord(event.target.dataset.letter);
 
-  if (!letterFound) {
-    (scoreValue - 1 < 0) ? scoreValue = 0 : scoreValue--;
-    strikes++;
+	if (!letterFound) {
+		scoreValue - 1 < 0 ? (scoreValue = 0) : scoreValue--;
+		strikes++;
 
-    if (strikes < maxStrikes) {
-      hangmanLimbs[strikes - 1].style.display = 'block';
-    }
-    else {
-      revealedWord = currentWord;
-      wordNotFound();
-    }
-  }
-  else {
-    scoreValue++;
-    if (!revealedWord.includes('_')) {
-      wordFound()
-    }
-  }
+		if (strikes < maxStrikes) {
+			hangmanLimbs[strikes - 1].style.display = "block";
+		} else {
+			revealedWord = currentWord;
+			wordNotFound();
+		}
+	} else {
+		scoreValue++;
+		if (!revealedWord.includes("_")) {
+			wordFound();
+		}
+	}
 
-  updateHighScore();
-  updateDisplay();
+	updateHighScore();
+	updateDisplay();
 }
 
 function findLetterInWord(letter) {
-  let letterFound = false;
+	let letterFound = false;
 
-  for (let i = 0; i < currentWord.length; ++i) {
+	for (let i = 0; i < currentWord.length; ++i) {
+		if (currentWord[i] === letter) {
+			letterFound = true;
+			revealedWord = setCharAt(revealedWord, i, currentWord[i]);
+		}
+	}
 
-    if (currentWord[i] === letter) {
-      letterFound = true;
-      revealedWord = setCharAt(revealedWord, i, currentWord[i]);
-    }
-  }
-
-  return letterFound;
+	return letterFound;
 }
 
 function wordFound() {
-  updateHighScore();
+	updateHighScore();
 
-  message.innerText = `Congratutions! word was ${currentWord.toLowerCase()}`;
-  startGame();
+	message.innerText = `Congratutions! word was ${currentWord.toLowerCase()}`;
+	startGame();
 }
 
 function wordNotFound() {
-  updateHighScore();
-  scoreValue = 0;
+	updateHighScore();
+	scoreValue = 0;
 
-  message.innerText = `Ouch! word was ${currentWord.toLowerCase()}`;
-  startGame();
+	message.innerText = `Ouch! word was ${currentWord.toLowerCase()}`;
+	startGame();
 }
 
 function updateHighScore() {
-  if (highScoreValue < scoreValue) {
-    highScoreValue = scoreValue;
-  }
+	if (highScoreValue < scoreValue) {
+		highScoreValue = scoreValue;
+	}
 
-  localStorage.setItem('highScore', highScoreValue);
+	localStorage.setItem("highScore", highScoreValue);
 }
 
 function updateDisplay() {
-  word.innerText = revealedWord;
-  updateScoreDisplay();
+	word.innerText = revealedWord;
+	updateScoreDisplay();
 }
 
 function updateScoreDisplay() {
-  score.innerText = scoreValue;
-  highScore.innerText = highScoreValue;
+	score.innerText = scoreValue;
+	highScore.innerText = highScoreValue;
 }
 
 // GAME NAVIGATION FUNTIONS:
 
 function goToMainMenu() {
-  hideAllScreens();
-  mainMenuContentContainer.style.display = 'flex';
+	hideAllScreens();
+	mainMenuContentContainer.style.display = "flex";
 }
 
 function goToCategorySelectionMenu() {
-  hideAllScreens();
-  categoryMenuContentContainer.style.display = 'flex';
+	hideAllScreens();
+	categoryMenuContentContainer.style.display = "flex";
 }
 
 function goToLoadingScreen() {
-  hideAllScreens();
-  loadingScreenContentContainer.style.display = 'block';
+	hideAllScreens();
+	loadingScreenContentContainer.style.display = "block";
 }
 
 function goToMainGame() {
-  hideAllScreens();
-  mainGameContainer.style.display = 'block';
+	hideAllScreens();
+	mainGameContainer.style.display = "block";
 }
 
 // helper functions
 
 function setCharAt(str, index, chr) {
-  if (index > str.length - 1) return str;
-  return str.substring(0, index) + chr + str.substring(index + 1);
+	if (index > str.length - 1) return str;
+	return str.substring(0, index) + chr + str.substring(index + 1);
 }
 
 function hideAllScreens() {
-  for (let i = 0; i < contentContainers.length; ++i) {
-    contentContainers[i].style.display = 'none';
-  }
+	for (let i = 0; i < contentContainers.length; ++i) {
+		contentContainers[i].style.display = "none";
+	}
 }
 
 function capitalizeWordList() {
-  for (let i = 0; i < wordList.length; ++i) {
-    wordList[i] = wordList[i].toUpperCase();
-  }
+	for (let i = 0; i < wordList.length; ++i) {
+		wordList[i] = wordList[i].toUpperCase();
+	}
 }
 
 // navigation event listeners
 
-normalModeButton.addEventListener('click', goToCategorySelectionMenu);
-hardcoreModeButton.addEventListener('click', (event) => {
-  initHardcoreMode();
+normalModeButton.addEventListener("click", goToCategorySelectionMenu);
+hardcoreModeButton.addEventListener("click", (event) => {
+	initHardcoreMode();
 });
 
 for (let i = 0; i < categoryButtons.length; ++i) {
-  categoryButtons[i].addEventListener('click', (event) => {
-    initNormalMode(event.target.dataset.name);
-  });
+	categoryButtons[i].addEventListener("click", (event) => {
+		initNormalMode(event.target.dataset.name);
+	});
 }
 
-categoryBackButton.addEventListener('click', goToMainMenu);
-mainGameHomeButton.addEventListener('click', goToMainMenu);
-
+categoryBackButton.addEventListener("click", goToMainMenu);
+mainGameHomeButton.addEventListener("click", goToMainMenu);
 
 createLetterButtons();
 initMainGameEventListeners();
 goToMainMenu();
 
-if (!localStorage.getItem('highScore')) {
-  localStorage.setItem('highScore', highScoreValue);
+if (!localStorage.getItem("highScore")) {
+	localStorage.setItem("highScore", highScoreValue);
 }
